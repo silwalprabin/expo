e085bb33ddfd7ea8df14fbc76e108f0c354bf440
Wojciech Dróżdż <behenate@gmail.com>
2026-06-24 18:22:47 +0200

    Add cli tool

--- packages/expo-app-intents/plugin/build/index.d.ts
+++
@@ -1,4 +0,0 @@
-import { Props } from './withAppIntents';
-declare const _default: (props?: Props) => [string, Props];
-export default _default;
-export { default as withAppIntents } from './withAppIntents';
